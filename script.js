c7a94fe6dba65177124f79409dac5bd7c771c867
Eduardo Fuentes <fuentes.eduardo19@gmail.com>
2020-10-10 12:49:47 -0700

    add javascript for home buttons and changed logo

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const navbar = document.querySelector(".navbar");
 menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('change');
 })
+
+
+const btn = document.querySelector("#btn");
+const container = document.querySelector(".container");
+const secondContainer = document.querySelector(".second-container");
+btn.addEventListener('click', () => {
+    container.classList.toggle('block');
+    secondContainer.classList.add("appear");
+})
+
+const home = document.querySelector('#home');
+home.addEventListener('click', () => {
+    container.classList.remove('block');
+    secondContainer.classList.remove("appear");
+})
