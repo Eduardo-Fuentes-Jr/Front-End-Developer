00264edaa6497933c0448b7a488584c3bbf05860
Eduardo Fuentes <fuentes.eduardo19@gmail.com>
2020-10-13 21:35:41 -0700

    Made links work with javascript

--- script.js
+++ script.js
@@ -21,3 +21,29 @@ home.addEventListener('click', () => {
     secondContainer.classList.remove("appear");
 })
 
+const about = document.getElementById('about');
+
+about.addEventListener('click', function() {
+    secondContainer.classList.add("appear");
+    container.classList.add('block');
+    const aboutSection = document.querySelector('.about-section');
+    aboutSection.scrollIntoView();
+})
+
+const skills = document.getElementById('skills');
+
+skills.addEventListener('click', function(){
+    secondContainer.classList.add("appear");
+    container.classList.add('block');
+    const skillsSection = document.querySelector('.skills-section');
+    skillsSection.scrollIntoView();
+})
+
+const portfolio = document.getElementById('portfolio');
+
+portfolio.addEventListener('click', function() {
+    secondContainer.classList.add("appear");
+    container.classList.add('block');
+    const portfolioSection = document.querySelector('.portfolio-section');
+    portfolioSection.scrollIntoView();
+})
